09880c41c997093df438b0de45054104c9d12264
Antonio <cuentadisponible89@gmail.com>
2023-07-20 16:21:09 +0200

    Initial commit

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace CV_RF_Rep
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
